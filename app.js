e9bc47e63b507e929fbebfca1650241e338d9750
joneyphair <joneyphair@qq.com>
2017-02-04 23:51:41 +0800

    fix

--- app.js
+++ app.js
@@ -2,9 +2,33 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const json = require('express-json');
 const useragent = require('express-useragent');
+const path = require('path')
+var dust = require('express-dustjs')
 
 const app = express();
 
+
+
+// Dustjs settings 
+dust._.optimizers.format = function (ctx, node) {
+  return node
+}
+ 
+// Define custom Dustjs helper 
+dust._.helpers.demo = function (chk, ctx, bodies, params) {
+  return chk.w('demo')
+}
+
+
+app.engine('html', dust.engine({
+  // Use dustjs-helpers 
+  useHelpers: true
+}));
+
+app.set('view engine', 'html');
+
+app.set('views', path.resolve(__dirname, './views'))
+ 
 
 app.use(useragent.express());
 app.use(bodyParser.json());
--- src/Controller/HomeController.js
+++ src/Controller/HomeController.js
@@ -20,21 +20,13 @@ class HomeController {
 
 		 	res.setHeader('Content-Type', 'application/json');
 
-			var params = {
-				method:'kdt.items.onsale.get',
-			}
-
+	
 
 			//res.redirect(url);
 
 			//res.json(params);
 
-			YouZan.request(params).then(function(response){
-				res.json(response);
-			}).catch(function(err){
-				res.send(err);
-			});
-	    //res.send('====>>>aaagaaHello world!');
+	    res.render('index');
 	}
 
 }
